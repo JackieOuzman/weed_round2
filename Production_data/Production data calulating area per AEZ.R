1fee72eaf514478eba073dedf0f3795eab1ba769
JackieOuzman <jackie.ouzman@csiro.au>
2024-03-21 17:20:30 +1030

    more files

---
+++ Production_data/Production data calulating area per AEZ.R
@@ -0,0 +1,40 @@
+library(tidyverse)
+library(readxl)
+library(dplyr)
+library(ggplot2)
+library(plotly)
+library(ggpubr)
+library(DT)
+
+library(lubridate)
+
+production_data <- read.csv("//FSSA2-ADL/clw-share1/Microlab/Economic impact of weeds round 2/production_data/1.Input_data_2024_model/production_data_AEZ.csv")
+
+
+#tidy up data frame
+str(production_data)
+production_data <- production_data %>% 
+  select(Unit:AEZ_code, mean_19_21)
+
+
+unique(production_data$Unit)
+## we want $ per tonne
+
+production_data_ha <- production_data %>% 
+  filter(Unit == "HA")
+
+unique(production_data_ha$Crop)
+
+
+production_data_ha <- production_data_ha %>% 
+  mutate(Crop = case_when(
+    Crop == "Grain Sorghum" ~ "Sorghum",
+    Crop == "Pulses" ~ "Pulse",
+    .default = Crop
+  ))
+
+
+##############################################################################
+
+write.csv(production_data_ha, 
+          "W:/Economic impact of weeds round 2/production_data/1.Input_data_2024_model/production_data_ha_AEZ.csv" ,row.names = FALSE)
